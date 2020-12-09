e4db402e04d9e9a41b1a75e2a81b8404a486936b
smdaa <saadmdaa03@gmail.com>
2020-12-09 10:58:27 +0100

    7liwa

--- sourceEtu/passeCodeRatToTam.ml
+++ sourceEtu/passeCodeRatToTam.ml
@@ -28,7 +28,7 @@ let rec analyse_code_expression e =
     begin
       match info_ast_to_info info with 
       | InfoVar(_, t, dep, reg) -> "LOAD (" ^ (string_of_int (getTaille t)) ^ ") " ^ (string_of_int dep) ^ "[" ^ reg ^ "]\n"
-      | InfoConst (_, i) -> "LOADL " ^ (string_of_int i)
+      | InfoConst (_, i) -> "LOADL " ^ (string_of_int i) ^ "\n"
       | _ -> failwith "erreur interne"
     end
   | True -> "LOADL 1\n"
@@ -44,7 +44,7 @@ let rec analyse_code_expression e =
       | PlusRat -> "CALL (SB) RAdd\n"
       | MultRat -> "CALL (SB) RMul\n"
       | EquInt -> "SUBR IEq\n"
-      | EquBool -> "SUBR IEq\n"
+      | EquBool -> "SUBR BEq\n"
       | Inf -> "SUBR ILss\n"
     end
 
@@ -106,7 +106,7 @@ let analyse_code_fonction (AstPlacement.Fonction(info, _, li, e)) =
     let taille_variables_locales = List.fold_right (fun i ti -> (taille_variables_declarees i) + ti) li 0 in 
     let taille_return = getTaille typeRet in
     let taille_parametres = List.fold_right (fun i ti -> ti + (getTaille i) ) typeParams 0  in
-    let nli = String.concat "" (List.map analyse_code_instruction li) in
+    let nli = (analyse_code_li li) in
     non ^ "\n" ^
     nli ^
     (analyse_code_expression e) ^
